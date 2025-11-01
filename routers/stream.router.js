b6cf196d2960d38393155317c6e6e7de3915b6c5
Talha Haider <talhahaider074@gmail.com>
2025-11-01 23:23:47 +0500

    server restarted commit

--- routers/stream.router.js
+++ routers/stream.router.js
@@ -1,7 +1,6 @@
 const { multipleupload } = require("../config/multer.config")
 const {createStream, getActive, getSingle, endStream, getCreatorActiveStream, getToken, createMessage, getMessages } = require("../services/stream.service")
 const router = require("express").Router()
-
 router.post("/create",multipleupload.single("image"),createStream)
 router.get("/active",getActive)
 router.get("/single/:id",getSingle)
